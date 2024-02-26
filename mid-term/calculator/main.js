5aac6302249e521eb16dac0b9f2ef7a650f778bf
githwizardn <103641200+githwizardn@users.noreply.github.com>
2024-02-26 20:17:19 +0400

    Add files via upload

---
+++ mid-term/calculator/main.js
@@ -0,0 +1,38 @@
+// მონაცემების შეყვანა
+let num1 = prompt("შეიყვანეთ პირველი რიცხვი:");
+let num2 = prompt("შეიყვანეთ მეორე რიცხვი:");
+let operation = prompt("შეიყვანეთ ოპერაცია (+, -, *, /):");
+
+//დადასტურება რიცხვი არის თუ არა
+function isNumber(value) {
+  return !isNaN(value);
+}
+
+// გამოთვლა
+function calculate(number1, number2, operation) {
+  switch (operation) {
+    case "+":
+      return number1 + number2;
+    case "-":
+      return number1 - number2;
+    case "*":
+      return number1 * number2;
+    case "/":
+      if (number2 !== 0) {
+        return number1 / number2;
+      } else {
+        return "შეცდომა : ნულზე გაყოფა";
+      }
+    default:
+      return "მცდარი ოპერაცია";
+  }
+}
+
+if (isNumber(num1) && isNumber(num2)) {
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  let result = calculate(num1, num2, operation);
+  console.log(`რეზულტატი: ${result}`);
+} else {
+  console.log("მცდარი ოპერაცია: დარწმუნდით რომ შეგყავთ რიცხვები.");
+}
---
+++ mid-term/jeirani/main.js
@@ -0,0 +1,47 @@
+function playGame() {
+  // არჩევა
+  var userChoice = prompt("აირჩიე ქვა,ქაღალდი ან მაკრატელი:");
+
+  // არჩევნის შემოწმება
+  if (
+    userChoice !== "ქვა" &&
+    userChoice !== "ქაღალდი" &&
+    userChoice !== "მაკრატელი"
+  ) {
+    alert("გთხოვთ შეიყვანოთ ზუსტად ისე როგორც ნაჩვენებია ");
+    console.log("გთხოვთ შეიყვანოთ ზუსტად ისე როგორც ნაჩვენებია ");
+
+    return; // ფუნქცის დახურვა თუ არასწორადაა შეყვანილი არჩევანი
+  }
+
+  // კომპიუტერის არჩევნის გენერირება
+  var choices = ["ქვა", "ქაღალდი", "მაკრატელი"];
+  var computerChoice = choices[Math.floor(Math.random() * 3)];
+
+  // არჩეევნის გამოცხადება
+  alert(
+    "შენ აირჩიე " + userChoice + ".\nკომპიუტერმა აირჩია " + computerChoice + "."
+  );
+  console.log(
+    "შენ აირჩიე " + userChoice + ".\nკომპიუტერმა აირჩია " + computerChoice + "."
+  );
+
+  // გამარჯვებულის განსაზღვრა
+  if (userChoice === computerChoice) {
+    alert("ფრეა!");
+    console.log("ფრეა!");
+  } else if (
+    (userChoice === "ქვა" && computerChoice === "მაკრატელი") ||
+    (userChoice === "ქაღალდი" && computerChoice === "ქვა") ||
+    (userChoice === "მაკრატელი" && computerChoice === "ქაღალდი")
+  ) {
+    alert("შენ მოიგე!");
+    console.log("შენ მოიგე!");
+  } else {
+    alert("კომპპიუტერმა მოიგო!");
+    console.log("კომპპიუტერმა მოიგო!");
+  }
+}
+
+// თამაშის დაწყება
+playGame();
