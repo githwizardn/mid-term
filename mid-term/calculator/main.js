b35bcb1b70cbcad5e9eed867a4086643dc00009c
githwizardn <103641200+githwizardn@users.noreply.github.com>
2024-02-29 17:00:42 +0400

    Update main.js

--- mid-term/calculator/main.js
+++ mid-term/calculator/main.js
@@ -1,43 +1,43 @@
 // მონაცემების შეყვანა
 let num1 = prompt("შეიყვანეთ პირველი რიცხვი:");
+let operation = prompt("შეიყვანეთ ოპერაცია (+, -, *, /, **, %):");
 let num2 = prompt("შეიყვანეთ მეორე რიცხვი:");
-let operation = prompt("შეიყვანეთ ოპერაცია (+, -, *, /):");
 
-//დადასტურება რიცხვი არის თუ არა
-function isNumber(value) {
-  return !isNaN(value);
-}
-
-// გამოთვლა
-function calculate(number1, number2, operation) {
-  switch (operation) {
-    case "+":
-      return number1 + number2;
-    case "-":
-      return number1 - number2;
-      case "**":
-      return number1 ** number2;
-
-    case "%":
-      return number1 % number2;
-    case "*":
-      return number1 * number2;
-    case "/":
-      if (number2 !== 0) {
-        return number1 / number2;
-      } else {
-        return "შეცდომა : ნულზე გაყოფა";
-      }
-    default:
-      return "მცდარი ოპერაცია";
-  }
-}
-
-if (isNumber(num1) && isNumber(num2)) {
+// დადასტურება რიცხვი არის თუ არა
+if (!isNaN(num1) && !isNaN(num2)) {
   num1 = parseFloat(num1);
   num2 = parseFloat(num2);
-  let result = calculate(num1, num2, operation);
+
+  // გამოთვლა
+  let result;
+  switch (operation) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      if (num2 !== 0) {
+        result = num1 / num2;
+      } else {
+        result = "შეცდომა: ნულზე გაყოფა";
+      }
+      break;
+    case "**":
+      result = num1 ** num2;
+      break;
+    case "%":
+      result = num1 % num2;
+      break;
+    default:
+      result = "მცდარი ოპერაცია";
+  }
   console.log(`რეზულტატი: ${result}`);
+  alert(`რეზულტატი: ${result}`);
 } else {
   console.log("მცდარი ოპერაცია: დარწმუნდით რომ შეგყავთ რიცხვები.");
 }
